d161867022ebf161a83f85099e344d3042b66eae
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2020-05-20 19:02:40 +0530

    Errors fixed.

--- classify_images.py
+++ classify_images.py
@@ -67,7 +67,6 @@ def classify_images(images_dir, results_dic, model):
     """
     for key in results_dic:
         model_label = (classifier((images_dir + key) , model)).lower().strip()
-        #model_label=model_label.lower().strip()
         truth = results_dic[key][0]
         if truth in model_label:
             results_dic[key].extend([model_label,1])
